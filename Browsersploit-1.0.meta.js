346b5fc2ac0297308afbe24cf81ede7456449176
natureless <aronas1>
2025-06-21 14:02:02 +0200

    useless commit

---
+++ Browsersploit-1.0.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Browsersploit
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Simple exploit for your browser
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
